55196fc8c0bcd768183d7d35c6724e54b867b505
Juan Carlos Pusch <108437005+carlos-pusch@users.noreply.github.com>
2026-02-24 00:23:55 +0100

    Update settings.py

--- wahlrechner/core/settings.py
+++ wahlrechner/core/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'wahlrechner.middleware.MaintenanceModeMiddleware',
 ]
 
 ROOT_URLCONF = "core.urls"
