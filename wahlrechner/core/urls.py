78d9d0da56e06d0c6e711baa2aea18dc68e1d954
Juan Carlos Pusch <108437005+carlos-pusch@users.noreply.github.com>
2026-04-10 17:28:25 +0200

    Update urls.py

--- wahlrechner/core/urls.py
+++ wahlrechner/core/urls.py
@@ -4,17 +4,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from wahlrechner.views import bulk_upload # lz_c_1: Bulk-Upload-View importieren
+
 # lz_b_1: Die feste URL_PREFIX wird entfernt, stattdessen dynamischer Slug
 
 urlpatterns = [
-    # Admin bleibt global erreichbar (z.B. unter /admin)
+    path('admin/bulk-upload/', bulk_upload, name='bulk_upload'),
     path('admin/', admin.site.urls),
-
-    # lz_b_1: Alle wahlrechner-Pfade beginnen mit dem Slug der Wahl
     path('<slug:wahl_slug>/', include('wahlrechner.tenant_urls')),
 ]
 
-# Statische und Medien-Dateien im Development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
